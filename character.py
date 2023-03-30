dd7afed037a1203ee9c4c2781ca9a98c9a894e15
miyeong <kmy9810@naver.com>
2023-03-30 22:18:45 +0900

    준최종입니닷

--- character.py
+++ character.py
@@ -1,9 +1,11 @@
 import random
-# 출력 색상 변경 
+
+# 출력 색상 변경
 R = "\033[91m"
 B = "\033[94m"
 W = "\033[0m"
 G = "\033[92m"
+
 
 # 플레이어, 몬스터 부모 클래스
 # 민경님
@@ -23,8 +25,8 @@ class Character:
 
     # 내가 조아하는 우리 팀장님!!! 제일 뼈대가 되는 구간을 너무 잘 짜신 것 같아요!!! 도움이 필요하면 언제든지 부르겠습니닷^ㅇ^ 화이팅 팀장님~~ - 묭
     def normal_attack(self, target):  # 기본 공격
-        if target.hp > 0:  
-            critical_attack = random.random() < 0.1 # 10퍼센트 확률로 치명타 발동
+        if target.hp > 0:
+            critical_attack = random.random() < 0.1  # 10퍼센트 확률로 치명타 발동
             if critical_attack:
                 damage = random.randint(int(self.normal_power * 0.8), int(self.normal_power * 1.2)) * 2
                 print("----------------------------\n 치명타 발동!!!!! \n----------------------------")
@@ -44,11 +46,11 @@ class Character:
         if self.hp <= 0:
             self.alive = False
         return 1
-            
-            
+
+
 # 혜민님
 class Party(Character):
-    def __init__(self, name, character, mp=100, hp=100, normal_power=100, magic_power=5, exp=0 , level=1):
+    def __init__(self, name, character, mp=100, hp=100, normal_power=100, magic_power=5, exp=0, level=1):
         super().__init__(name)
         self.character = character
         self.mp = mp
@@ -68,28 +70,26 @@ class Party(Character):
 
     # 혜민
     def item(self):
-        items_dic = { 
-                    "1" : steelsword ,
-                    "2" : armor    ,
-                    "3" : hp_portion ,
-                    "4" : mp_portion 
-                    }
+        items_dic = {
+            "1": steelsword,
+            "2": armor,
+            "3": hp_portion,
+            "4": mp_portion
+        }
         num = input(
-                            "1.steelsword\n"
-                            "2.armor\n"
-                            "3.hp_portion\n"
-                            "4.mp_portion\n"
-                            "아이템을 선택해주세요 : ")
+            "1.steelsword\n"
+            "2.armor\n"
+            "3.hp_portion\n"
+            "4.mp_portion\n"
+            "아이템을 선택해주세요 : ")
         if num not in items_dic.keys():
             print("올바른 아이템을 선택해주세요.")
             return self.item()
-        
+
         # 강철검 : 착용 시 파워 5 증가
         # 갑옷 : 착용 시 HP 50 증가
         # HP 포션 : 획득(사용) 시 HP 전부 회복
         # MP 포션 : 획득(사용) 시 MP 전부 회복
-        
-
 
     def success_hunt(self):
         # 몬스터 사냥에 성공 시 다음중 하나 또는 전부를 획득 합니다.
@@ -108,55 +108,56 @@ class Party(Character):
         # 100경험치 달성시 레벨업!
         # 레벨업  -> 파워 2 증가, 체력 전부 회복
         # 다시 경험치는 0으로 설정
-        
-        
+
 
 # items----------------------------
 def steelsword():
-            if Party.power >= 50:
-                pass
-            elif Party.power < 50:
-                Party.power = 50
-            else:
-                Party.power += 5
-    
+    if Party.power >= 50:
+        pass
+    elif Party.power < 50:
+        Party.power = 50
+    else:
+        Party.power += 5
+
+
 def armor():
-            if Party.hp >= 100:
-                pass
-            elif Party.hp > 50:
-                Party.hp = 100
-            else:
-                Party.hp += 50
-    
+    if Party.hp >= 100:
+        pass
+    elif Party.hp > 50:
+        Party.hp = 100
+    else:
+        Party.hp += 50
+
+
 def hp_portion():
-            Party.hp = 100
-        
+    Party.hp = 100
+
+
 def mp_portion():
-            Party.mp = 100       
+    Party.mp = 100
 
 
 # items--------end-------------------
-       
 
 
 #### ------------------------Monster 시작
 #### -----------------------------------
 
-#1. monster에서 쓰고 있던 normal_attack 제거중... 완료
-#2. Character에서 상속받을 수 있는 건 받게 만드는 중...
+# 1. monster에서 쓰고 있던 normal_attack 제거중... 완료
+# 2. Character에서 상속받을 수 있는 건 받게 만드는 중...
 # __init__ 완료
 # 3. 코드 간소화 중...
 class Monster(Character):
     # 몬스터 스킬 사용 코드 -> 랜덤을 사용 하거나, mp나 sp도 좋을 듯!
     # 1차 목표 스킬 구현.
     # 가능하면 여러 가지 스킬 구현.
-    #몬스터 스킬의 필요한 mp량 나중에 수정을 쉽게 하기 위해 변수로 추가해 둠.
-    monster_skill_mp = [10, 20, 30] #약공 강공 힐
+    # 몬스터 스킬의 필요한 mp량 나중에 수정을 쉽게 하기 위해 변수로 추가해 둠.
+    monster_skill_mp = [10, 20, 30]  # 약공 강공 힐
 
-    def __init__(self, name, hp, normal_power, mp, magic_power): #normal attack 제거 job도 일단 제거
+    def __init__(self, name, hp, normal_power, mp, magic_power):  # normal attack 제거 job도 일단 제거
         # 이름, hp, nomral_power, 일반 공격, 직업
         # 따로 함수로..고민중!()
-        
+
         super().__init__(name, hp, normal_power, mp)
         # self.normal_attack = normal_attack
         # self.job = job
@@ -165,33 +166,34 @@ class Monster(Character):
         self.alive = True
 
     # Character class에 추가 예정!
-    def enough_mp(self, required_mp): #mp가 충분한지 확인하는 함수. 좀 짧게 만드려고 추가.
+    def enough_mp(self, required_mp):  # mp가 충분한지 확인하는 함수. 좀 짧게 만드려고 추가.
         skill_attack_flag = 1
         if self.mp < required_mp:
             skill_attack_flag = 0
         return skill_attack_flag
 
     def choose_skill(self):
-        possible_skill_list = [0,0,0] #스킬이 사용가능한 상황인지 체크하려고 만듦. + 숫자를 추가해서 확률을 높이려고 함.
-        for index in range(len(Monster.monster_skill_mp)-1):
+        possible_skill_list = [0, 0, 0]  # 스킬이 사용가능한 상황인지 체크하려고 만듦. + 숫자를 추가해서 확률을 높이려고 함.
+        for index in range(len(Monster.monster_skill_mp) - 1):
             if Monster.enough_mp(self, Monster.monster_skill_mp[index]) == 1:
                 possible_skill_list[index] = 1
-        if (self.hp <= (self.max_hp//2)) and (Monster.enough_mp(self, Monster.monster_skill_mp[2])): #체력이 50%이하 + mp가 충분하면 힐 할 확률 생성
+        if (self.hp <= (self.max_hp // 2)) and (
+        Monster.enough_mp(self, Monster.monster_skill_mp[2])):  # 체력이 50%이하 + mp가 충분하면 힐 할 확률 생성
             possible_skill_list[2] += 1
-        if self.mp >= (Monster.monster_skill_mp[1] * 5) : 
-            possible_skill_list[1] += 2 #mp가 강공보다 5배 많다면 2만큼 확률 추가
-        elif self.mp >= (Monster.monster_skill_mp[1] * 3) : 
-            possible_skill_list[1] += 1 #mp가 강공보다 3배 많다면 1만큼 확률 추가
-        elif self.mp >= (Monster.monster_skill_mp[0] * 5) :
-            possible_skill_list[0] += 2 #mp가 강공보다 3배 많다면 1만큼 확률 추가
-        elif self.mp >= (Monster.monster_skill_mp[0] * 3) :
-            possible_skill_list[0] += 1 #mp가 강공보다 3배 많다면 1만큼 확률 추가
-        
+        if self.mp >= (Monster.monster_skill_mp[1] * 5):
+            possible_skill_list[1] += 2  # mp가 강공보다 5배 많다면 2만큼 확률 추가
+        elif self.mp >= (Monster.monster_skill_mp[1] * 3):
+            possible_skill_list[1] += 1  # mp가 강공보다 3배 많다면 1만큼 확률 추가
+        elif self.mp >= (Monster.monster_skill_mp[0] * 5):
+            possible_skill_list[0] += 2  # mp가 강공보다 3배 많다면 1만큼 확률 추가
+        elif self.mp >= (Monster.monster_skill_mp[0] * 3):
+            possible_skill_list[0] += 1  # mp가 강공보다 3배 많다면 1만큼 확률 추가
+
         total_chance = 0
         for chance in possible_skill_list:
             total_chance += chance
-        choose_chance = random.randint(1,total_chance)
-        choose_result = 0 #선택할 스킬
+        choose_chance = random.randint(1, total_chance)
+        choose_result = 0  # 선택할 스킬
         for chance in possible_skill_list:
             choose_result += 1
             if choose_chance <= chance:
@@ -200,14 +202,12 @@ class Monster(Character):
                 choose_chance -= chance
         return choose_result
 
-    
     def skill_attack(self, other):
         # 랜덤 추가 고려중!~~~^ㅇ^ ##했습니다. :D
         skill_type = Monster.choose_skill(self)
 
-
-        if skill_type == 1: # 1은 약한 스킬 2는 강한 스킬 3은 회복. 3이 획복이라면 함수 이름 변경 필요할 듯.
-            damage = random.randint(self.nomral_power, self.nomral_power*2+1) #데미지가 nomral_power ~ nomral_power*2
+        if skill_type == 1:  # 1은 약한 스킬 2는 강한 스킬 3은 회복. 3이 획복이라면 함수 이름 변경 필요할 듯.
+            damage = random.randint(self.nomral_power, self.nomral_power * 2 + 1)  # 데미지가 nomral_power ~ nomral_power*2
             if self.mp < 10:
                 self.normal_attack(other)
             else:
@@ -218,7 +218,8 @@ class Monster(Character):
                     self.alive = False
                     print(f"{other.name}이(가) 쓰러졌습니다.")
         elif skill_type == 2:
-            damage = random.randint(self.nomral_power*2, self.nomral_power*3+1) #데미지가 nomral_power*2 ~ nomral_power*3
+            damage = random.randint(self.nomral_power * 2,
+                                    self.nomral_power * 3 + 1)  # 데미지가 nomral_power*2 ~ nomral_power*3
             if self.mp < 20:
                 self.normal_attack(other)
             else:
@@ -229,12 +230,13 @@ class Monster(Character):
                     self.alive = False
                     print(f"{other.name}이(가) 쓰러졌습니다.")
         elif skill_type == 3:
-            heal = random.randint(self.nomral_power//2, self.nomral_power+1) #1/2~1만큼 회복
+            heal = random.randint(self.nomral_power // 2, self.nomral_power + 1)  # 1/2~1만큼 회복
             if self.mp < 10:
                 self.normal_attack(other)
             else:
                 self.hp += heal
                 print(f"{self.name}이 {heal}만큼 회복해 hp가 {self.hp}가 되었다!")
+
 
 '''
 그냥 간단히 만들어만 놓은 몬스터 사냥시 보상 함수.
@@ -253,79 +255,84 @@ class Monster(Character):
             self.alive = False
             print(f"{other.name}이(가) 쓰러졌습니다.")
 '''
-        
 
-#몬스터를 그룹화 해서 묶어주기. 아마 리스트나 딕셔너리로 구현하게 될 것 같음.
-#Monster 클래스는 단일 몬스터의 객체로 남겨두려고 함.
-#그래서 따로 그룹을 형성하는 함수를 만들려고 함.
-#리스트로 생성하려고 함. 처음에는 딕셔너리로 생성하려고 했지만 이미 필요한 정보들이 생성될 몬스터 객체에 들어있어서 리스트로 구성원 체크.
+# 몬스터를 그룹화 해서 묶어주기. 아마 리스트나 딕셔너리로 구현하게 될 것 같음.
+# Monster 클래스는 단일 몬스터의 객체로 남겨두려고 함.
+# 그래서 따로 그룹을 형성하는 함수를 만들려고 함.
+# 리스트로 생성하려고 함. 처음에는 딕셔너리로 생성하려고 했지만 이미 필요한 정보들이 생성될 몬스터 객체에 들어있어서 리스트로 구성원 체크.
 test_fl = 1
 test_gd = 1
-def monster_generation(floor_level, game_difficulty): #몬스터 인자 생성
-    monster_name =''
-    named_monster_flag = 0 #네임드 몬스터 결정하는 flag
-    name_named_monster_list = ['우두머리', '대장']
-    name_first_adjective_list = ['크고','', '작고']
-    name_second_adjective_list = ['강인한','평범한', '나약한', '병든']
-    name_monster_variation_list = ['멧돼지','돼지','닭']
 
-    name_first_adjective_int = random.randrange(0,3)
-    name_second_adjective_int = random.randrange(0,4)
-    name_monster_variation_int = random.randrange(0,3)
-    
-    check_named_monster_int = random.randint(0,9)
-    if check_named_monster_int == 0: #1/10 확률로 네임드 몬스터
+
+def monster_generation(floor_level, game_difficulty):  # 몬스터 인자 생성
+    monster_name = ''
+    named_monster_flag = 0  # 네임드 몬스터 결정하는 flag
+    name_named_monster_list = ['우두머리', '대장']
+    name_first_adjective_list = ['크고', '', '작고']
+    name_second_adjective_list = ['강인한', '평범한', '나약한', '병든']
+    name_monster_variation_list = ['멧돼지', '돼지', '닭']
+
+    name_first_adjective_int = random.randrange(0, 3)
+    name_second_adjective_int = random.randrange(0, 4)
+    name_monster_variation_int = random.randrange(0, 3)
+
+    check_named_monster_int = random.randint(0, 9)
+    if check_named_monster_int == 0:  # 1/10 확률로 네임드 몬스터
         named_monster_flag = 1
-    name_named_monster_int = random.randrange(0,2) 
+    name_named_monster_int = random.randrange(0, 2)
     if name_first_adjective_int == 1:
-        monster_name = name_second_adjective_list[name_second_adjective_int]+' '
+        monster_name = name_second_adjective_list[name_second_adjective_int] + ' '
     else:
-        monster_name = name_first_adjective_list[name_first_adjective_int]+' '+name_second_adjective_list[name_second_adjective_int]+' '
-    if named_monster_flag == 1 :
+        monster_name = name_first_adjective_list[name_first_adjective_int] + ' ' + name_second_adjective_list[
+            name_second_adjective_int] + ' '
+    if named_monster_flag == 1:
         monster_name += name_named_monster_list[name_named_monster_int]
     monster_name += name_monster_variation_list[name_monster_variation_int]
 
-    monster_base_hp = random.randrange(80,121) #몬스터 기본체력
-    monster_add_hp = ((3-name_first_adjective_int)+(4-name_second_adjective_int)+(3-name_monster_variation_int)+floor_level)*10*game_difficulty #이름에 따른 추가 체력. 레벨상승시 lvl*10*난이도
-    monster_total_hp = (monster_base_hp+monster_add_hp)*(named_monster_flag+1) #네임드일 경우 체력 2배
+    monster_base_hp = random.randrange(80, 121)  # 몬스터 기본체력
+    monster_add_hp = ((3 - name_first_adjective_int) + (4 - name_second_adjective_int) + (
+                3 - name_monster_variation_int) + floor_level) * 10 * game_difficulty  # 이름에 따른 추가 체력. 레벨상승시 lvl*10*난이도
+    monster_total_hp = (monster_base_hp + monster_add_hp) * (named_monster_flag + 1)  # 네임드일 경우 체력 2배
 
-    monster_base_power = random.randrange(8,13) #몬스터 기본파워
-    monster_add_power = ((3-name_first_adjective_int)+(4-name_second_adjective_int)+(3-name_monster_variation_int)+floor_level)*game_difficulty #이름에 따른 추가 파워. 레벨상승시 lvl*난이도
-    monster_total_power = (monster_base_power+monster_add_power)*(named_monster_flag+1) #네임드일 경우 2배의 파워
+    monster_base_power = random.randrange(8, 13)  # 몬스터 기본파워
+    monster_add_power = ((3 - name_first_adjective_int) + (4 - name_second_adjective_int) + (
+                3 - name_monster_variation_int) + floor_level) * game_difficulty  # 이름에 따른 추가 파워. 레벨상승시 lvl*난이도
+    monster_total_power = (monster_base_power + monster_add_power) * (named_monster_flag + 1)  # 네임드일 경우 2배의 파워
+
+    # 일단은 mp랑 magic_power는 hp랑 normal_power그대로 가져옴.
+    monster_base_mp = random.randrange(80, 121)  # 몬스터 기본mp
+    monster_add_mp = ((3 - name_first_adjective_int) + (4 - name_second_adjective_int) + (
+                3 - name_monster_variation_int) + floor_level) * 10 * game_difficulty  # 이름에 따른 추가 체력. 레벨상승시 lvl*10*난이도
+    monster_total_mp = (monster_base_hp + monster_add_mp) * (named_monster_flag + 1)  # 네임드일 경우 체력 2배
+
+    monster_base_magic_power = random.randrange(8, 13)  # 몬스터 기본파워
+    monster_add_magic_power = ((3 - name_first_adjective_int) + (4 - name_second_adjective_int) + (
+                3 - name_monster_variation_int) + floor_level) * game_difficulty  # 이름에 따른 추가 파워. 레벨상승시 lvl*난이도
+    monster_total_magic_power = (monster_base_magic_power + monster_add_magic_power) * (
+                named_monster_flag + 1)  # 네임드일 경우 2배의 파워
+
+    monster_base_xp = random.randrange(8, 13)  # 몬스터 기본파워
+    monster_add_xp = ((3 - name_first_adjective_int) + (4 - name_second_adjective_int) + (
+                3 - name_monster_variation_int) + floor_level) * game_difficulty  # 이름에 따른 추가 파워. 레벨상승시 lvl*난이도
+    monster_total_xp = (monster_base_xp + monster_add_xp) * (named_monster_flag + 1)  # 네임드일 경우 2배의 파워
+    # , monster_total_xp
+    return Monster(monster_name, monster_total_hp, monster_total_power, monster_total_mp,
+                   monster_total_magic_power)  # name, hp=100, normal_power=100, mp=50
 
 
-    #일단은 mp랑 magic_power는 hp랑 normal_power그대로 가져옴.
-    monster_base_mp = random.randrange(80,121) #몬스터 기본mp
-    monster_add_mp = ((3-name_first_adjective_int)+(4-name_second_adjective_int)+(3-name_monster_variation_int)+floor_level)*10*game_difficulty #이름에 따른 추가 체력. 레벨상승시 lvl*10*난이도
-    monster_total_mp = (monster_base_hp+monster_add_mp)*(named_monster_flag+1) #네임드일 경우 체력 2배
-
-    monster_base_magic_power = random.randrange(8,13) #몬스터 기본파워
-    monster_add_magic_power = ((3-name_first_adjective_int)+(4-name_second_adjective_int)+(3-name_monster_variation_int)+floor_level)*game_difficulty #이름에 따른 추가 파워. 레벨상승시 lvl*난이도
-    monster_total_magic_power = (monster_base_magic_power+monster_add_magic_power)*(named_monster_flag+1) #네임드일 경우 2배의 파워    
-
-    monster_base_xp = random.randrange(8,13) #몬스터 기본파워
-    monster_add_xp = ((3-name_first_adjective_int)+(4-name_second_adjective_int)+(3-name_monster_variation_int)+floor_level)*game_difficulty #이름에 따른 추가 파워. 레벨상승시 lvl*난이도
-    monster_total_xp = (monster_base_xp+monster_add_xp)*(named_monster_flag+1) #네임드일 경우 2배의 파워    
-    #, monster_total_xp
-    return Monster(monster_name, monster_total_hp, monster_total_power, monster_total_mp, monster_total_magic_power) # name, hp=100, normal_power=100, mp=50
-
-
-def monster_group(): #몬스터 리스트 생성
-    number_of_group = random.randint(1,3)+(2)//2 # 1~3마리 랜덤 생성 + 난이도와 도달한 층에 따라 그룹원 추가
+def monster_group():  # 몬스터 리스트 생성
+    number_of_group = random.randint(1, 3) + (2) // 2  # 1~3마리 랜덤 생성 + 난이도와 도달한 층에 따라 그룹원 추가
     monster_list = []
     for index in range(number_of_group):
-        new_monster = monster_generation(3,3) #Monster()안에 난이도와 계층 변수 필요.   
+        new_monster = monster_generation(3, 3)  # Monster()안에 난이도와 계층 변수 필요.
         monster_list.append(new_monster)
     return monster_list
+
 
 def monster_print(monster_list):
     for a in monster_list:
         a.show_status()
     return 1
-
-
-
-
 
 
 #### ------------------------Monster 끝
@@ -348,7 +355,6 @@ class Warrior(Party):
             print(f"\n {self.name}의 휠윈드! mp 10을 소모해 {len(monster_list)}마리의 몬스터에게 각각 {magic_damage}의 데미지를 입혔습니다!\n ")
         else:
             print("\n 마나가 부족합니다. \n")
-
 
 # class Wizard(Party):
 #     def __init__(self, name="마법사", character ="Wizard"):
@@ -444,7 +450,7 @@ class Warrior(Party):
 #         else:
 #             print("\n 마나가 부족합니다. \n")
 
-        # ----------테스트용 임시 몬스터 클래스---------
+# ----------테스트용 임시 몬스터 클래스---------
 
 # class Monster:
 
@@ -483,4 +489,3 @@ class Warrior(Party):
 
 
 #### ---------------------------------------------------------Job 끝
-
--- main.py
+++ main.py
@@ -1,27 +1,31 @@
 import character as ch
 import random
-#import sys
-# 출력 색상 변경 
+
+# import sys
+# 출력 색상 변경
 R = "\033[91m"
 B = "\033[94m"
 W = "\033[0m"
 G = "\033[92m"
 
-#타입 체크하는 함수
+
+# 타입 체크하는 함수
 def check_type(required_type, input_type):
     if required_type != input_type:
         print('잘못된 타입을 입력하셨습니다. 다시 입력해 주세요.')
-        # 재입력하는 함수 필요. 
+        # 재입력하는 함수 필요.
         return True
     else:
-        #통과했으니 그냥 그대로 가게 하면 될듯.
+        # 통과했으니 그냥 그대로 가게 하면 될듯.
         return False
+
+
 # 미영
 # 플레이어 생성
 def create_party():
     # 이름을 입력
     party_name = input("파티 이름을 정해주세요. \n"
-                        "파티 : ")
+                       "파티 : ")
 
     return party_name
 
@@ -32,9 +36,9 @@ def select_job(num):
     choice_job = []
     for i in range(num):
         # 직업 리스트에서 직업을 고르기
-        character_jobs = input(f"{i+1}번째 캐릭터의 직업을 선택해주세요. \n"
-                            f"1.Archer 2.Warrior 3.Wizard 4.Healer 5.Tanker \n"
-                            f"직업(숫자) : ")
+        character_jobs = input(f"{i + 1}번째 캐릭터의 직업을 선택해주세요. \n"
+                               f"1.Archer 2.Warrior 3.Wizard 4.Healer 5.Tanker \n"
+                               f"직업(숫자) : ")
         if job_list[int(character_jobs) - 1] in choice_job:
             print("이미 선택한 캐릭터 입니다! \n 처음부터 다시 선택해주세요!")  # 지금은 처음부터 다시 시작이지만 후에 수정예정
             return select_job(num)
@@ -57,15 +61,14 @@ def select_character():
     characters = party.character
     print('전투할 캐릭터를 선택해 주세요.')
     for i in range(len(party.character)):
-        print(f"{i+1}.{characters[i]}")
+        print(f"{i + 1}.{characters[i]}")
     answer = input("선택한 캐릭터 : ")
-    return characters[int(answer)-1]  # 캐릭터 딕셔너리에 키값으로 넣어서 return
+    return characters[int(answer) - 1]  # 캐릭터 딕셔너리에 키값으로 넣어서 return
 
 
 def show_attack():
-    
     answer = input("공격을 선택해 주세요. \n"
-                    "1.일반공격 \n2.스킬공격 \n3.아이템 사용 \n4.스탯 확인 \n")
+                   "1.일반공격 \n2.스킬공격 \n3.아이템 사용 \n4.스탯 확인 \n")
     print(monster)
     if answer == "1":
         attack_choice_character.normal_attack(monster_list[0])
@@ -92,7 +95,7 @@ def check_answer(num=0):
             return int(answer)
     if num == 0:
         answer = input("1.예 2.아니오 \n"
-                        "선택 : ")
+                       "선택 : ")
         if answer == "1":
             return "success"
         elif answer == "2":
@@ -104,17 +107,16 @@ def check_answer(num=0):
 
 
 character_list = {
-    'Warrior': ch.Warrior(name="Warrior",character="Warrior",hp=3000,mp=500,normal_power=300,magic_power=50),
+    'Warrior': ch.Warrior(name="Warrior", character="Warrior", hp=3000, mp=500, normal_power=300, magic_power=50),
     # 'Wizard': ch.Wizard(name="Warrior",character="Warrior",hp=300,mp=50,normal_power=30,magic_power=5),
     # 'Archer': ch.Archer(name="Warrior",character="Warrior",hp=300,mp=50,normal_power=30,magic_power=5),
     # 'Tanker': ch.Tanker(name="Warrior",character="Warrior",hp=300,mp=50,normal_power=30,magic_power=5),
     # 'Healer': ch.Healer(name="Warrior",character="Warrior",hp=300,mp=50,normal_power=30,magic_power=5),
-            }
+}
 
 game_end = 0  # 안쪽 while문이 break되어 게임을 끝내고 싶을 때 +1
 
 monster_list = ch.monster_group()
-
 
 # 파티 이름 정의 -> 미영
 party_name = create_party()  # 파티이름
@@ -125,7 +127,6 @@ character_job = select_job(party_member)  # 캐릭터의 job 설정
 party = ch.Party(party_name, character_job, hp=int(party_member))  # part 정의 / 파티명, 선택 캐릭 이름 리스트
 party.show_choice_character()  # 선택한 캐릭터를 보여줌
 
-
 # 미영
 while True:
     if game_end == 1:
@@ -133,31 +134,31 @@ while True:
     # 몬스터 종류 선택(물, 불, 바람, 돌 중 하나 선택)
     # choice_monster = select_monster()
     # monster = ch.Monster(choice_monster)
-    monster = ch.monster_generation(3,3)
+    monster = ch.monster_generation(3, 3)
     # 몬스터 스탯 확인
     ch.monster_print(monster_list)
     while True:
         # 플레이어 공격할 캐릭터 선택
         choice_character = select_character()
-        attack_choice_character = character_list[choice_character] # choice_character를 key값으로 사용해 직업 정의
+        attack_choice_character = character_list[choice_character]  # choice_character를 key값으로 사용해 직업 정의
         # attack_choice_character.skill_attack()  # 이게 된다고..?
         # 플레이어 공격 선택 (일반 공격, 직업별 스킬, 아이템 사용, 스탯 확인(몬스터,플레이어))
         show_attack()  # 플레이어 공격 포함한 함수
         ch.monster_print(monster_list)
-        if len(monster_list) == 0 :
+        if len(monster_list) == 0:
             print("모든 몬스터를 사냥하셨습니다.")
             game_end += 1
             break
         # 몬스터 공격
-        if monster_list[0].hp > 0: 
-            attack_type = random.randint(1,2)
-            if attack_type % 2 == 0: #일단은 그냥 스킬 쓸 확률 1/2로 해놨습니다. 
+        if monster_list[0].hp > 0:
+            attack_type = random.randint(1, 2)
+            if attack_type % 2 == 0:  # 일단은 그냥 스킬 쓸 확률 1/2로 해놨습니다.
                 monster_list[0].normal_attack(attack_choice_character)  # 유저의 캐릭터들이 모두 피가 0이되면 유저 사망.
             else:
                 monster_list[0].skill_attack(attack_choice_character)
         attack_choice_character.check_alive()
         monster.check_alive()
-        if monster_list[0].hp <= 0 :
+        if monster_list[0].hp <= 0:
             del monster_list[0]
         if not attack_choice_character.alive:
             party.hp -= 1  # 파티의 hp는 캐릭터 수와 같음!
@@ -188,7 +189,6 @@ while True:
             else:
                 game_end = 1
                 break
-    
 
 '''
 def check_type(required_type, input_type):
@@ -199,9 +199,9 @@ def check_type(required_type, input_type):
     else:
         #통과했으니 그냥 그대로 가게 하면 될듯.
         return True
-    
+
 do{
-    
+
     }
 while(check_type(변수1, 변수2))
 
